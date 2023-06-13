618157282a47b0343d88793d76c6b470c471f799
Augustine Mavor-Parker <augustine.mavor-parker@cs.ucl.ac.uk>
2023-06-14 00:50:31 +0100

    scaling up to more environments

--- manual_sweep.sh
+++ manual_sweep.sh
@@ -1,18 +1,35 @@
 seed_list=(5)
-learning_rate_list=(1e-2 1e-3 1e-4 1e-5)
-entropy_cost_list=(1e-2 1e-1 1e-3)
+number_of_cell_types_list=(64 32 16 4 2)
+learning_rate_list=(3e-4)
+entropy_cost_list=(1e-2)
 batch_size_list=(1024)
+env_name_list=("halfcheetah" "hopper" "walker2d")
+# env_name_list=("ant")
 
-
-for seed in ${seed_list[@]};
+for env_name in "${env_name_list[@]}";
 do
-    for learning_rate in ${learning_rate_list[@]};
+    for seed in "${seed_list[@]}";
     do
-        for entropy_cost in ${entropy_cost_list[@]};
+        python training_torch.py --env_name $env_name --seed $seed --is_weight_sharing False
+    done
+done
+
+
+for env_name in "${env_name_list[@]}";
+do
+    for seed in ${seed_list[@]};
+    do
+        for learning_rate in ${learning_rate_list[@]};
         do
-            for batch_size in ${batch_size_list[@]};
+            for entropy_cost in ${entropy_cost_list[@]};
             do
-                python training_torch.py --seed $seed --batch_size $batch_size --entropy_cost $entropy_cost --learning_rate $learning_rate
+                for batch_size in ${batch_size_list[@]};
+                do
+                    for number_of_cell_types in ${number_of_cell_types_list[@]};
+                    do
+                        python training_torch.py --env_name $env_name --is_weight_sharing True --seed $seed --batch_size $batch_size --entropy_cost $entropy_cost --learning_rate $learning_rate --number_of_cell_types $number_of_cell_types
+                    done
+                done
             done
         done
     done
--- training_torch.py
+++ training_torch.py
@@ -92,6 +92,183 @@ def main(args):
 
         def __init__(
             self,
+            policy_layers: Sequence[int],
+            value_layers: Sequence[int],
+            entropy_cost: float,
+            discounting: float,
+            reward_scaling: float,
+            device: str,
+        ):
+            super(Agent, self).__init__()
+
+            policy = []
+            for w1, w2 in zip(policy_layers, policy_layers[1:]):
+                policy.append(nn.Linear(w1, w2),)
+                policy.append(nn.SiLU())
+            policy.pop()  # drop the final activation
+            self.policy = nn.Sequential(*policy)
+
+            value = []
+            for w1, w2 in zip(value_layers, value_layers[1:]):
+                value.append(nn.Linear(w1, w2),)
+                value.append(nn.SiLU())
+            value.pop()  # drop the final activation
+            self.value = nn.Sequential(*value)
+
+            self.num_steps = torch.zeros((), device=device)
+            self.running_mean = torch.zeros(policy_layers[0], device=device)
+            self.running_variance = torch.zeros(policy_layers[0], device=device)
+
+            self.entropy_cost = entropy_cost
+            self.discounting = discounting
+            self.reward_scaling = reward_scaling
+            self.lambda_ = 0.95
+            self.epsilon = 0.3
+            self.device = device
+
+        @torch.jit.export
+        def dist_create(self, logits):
+            """Normal followed by tanh.
+
+            torch.distribution doesn't work with torch.jit, so we roll our own."""
+            loc, scale = torch.split(logits, logits.shape[-1] // 2, dim=-1)
+            scale = F.softplus(scale) + 0.001
+            return loc, scale
+
+        @torch.jit.export
+        def dist_sample_no_postprocess(self, loc, scale):
+            return torch.normal(loc, scale)
+
+        @classmethod
+        def dist_postprocess(cls, x):
+            return torch.tanh(x)
+
+        @torch.jit.export
+        def dist_entropy(self, loc, scale):
+            log_normalized = 0.5 * math.log(2 * math.pi) + torch.log(scale)
+            entropy = 0.5 + log_normalized
+            entropy = entropy * torch.ones_like(loc)
+            dist = torch.normal(loc, scale)
+            log_det_jacobian = 2 * (math.log(2) - dist - F.softplus(-2 * dist))
+            entropy = entropy + log_det_jacobian
+            return entropy.sum(dim=-1)
+
+        @torch.jit.export
+        def dist_log_prob(self, loc, scale, dist):
+            log_unnormalized = -0.5 * ((dist - loc) / scale).square()
+            log_normalized = 0.5 * math.log(2 * math.pi) + torch.log(scale)
+            log_det_jacobian = 2 * (math.log(2) - dist - F.softplus(-2 * dist))
+            log_prob = log_unnormalized - log_normalized - log_det_jacobian
+            return log_prob.sum(dim=-1)
+
+        @torch.jit.export
+        def update_normalization(self, observation):
+            self.num_steps += observation.shape[0] * observation.shape[1]
+            input_to_old_mean = observation - self.running_mean
+            mean_diff = torch.sum(input_to_old_mean / self.num_steps, dim=(0, 1))
+            self.running_mean = self.running_mean + mean_diff
+            input_to_new_mean = observation - self.running_mean
+            var_diff = torch.sum(input_to_new_mean * input_to_old_mean, dim=(0, 1))
+            self.running_variance = self.running_variance + var_diff
+
+        @torch.jit.export
+        def normalize(self, observation):
+            variance = self.running_variance / (self.num_steps + 1.0)
+            variance = torch.clip(variance, 1e-6, 1e6)
+            return ((observation - self.running_mean) / variance.sqrt()).clip(-5, 5)
+
+        @torch.jit.export
+        def get_logits_action(self, observation):
+            observation = self.normalize(observation)
+            logits = self.policy(observation)
+            loc, scale = self.dist_create(logits)
+            action = self.dist_sample_no_postprocess(loc, scale)
+            return logits, action
+
+        @torch.jit.export
+        def compute_gae(self, truncation, termination, reward, values, bootstrap_value):
+            truncation_mask = 1 - truncation
+            # Append bootstrapped value to get [v1, ..., v_t+1]
+            values_t_plus_1 = torch.cat(
+                [values[1:], torch.unsqueeze(bootstrap_value, 0)], dim=0
+            )
+            deltas = (
+                reward + self.discounting * (1 - termination) * values_t_plus_1 - values
+            )
+            deltas *= truncation_mask
+
+            acc = torch.zeros_like(bootstrap_value)
+            vs_minus_v_xs = torch.zeros_like(truncation_mask)
+
+            for ti in range(truncation_mask.shape[0]):
+                ti = truncation_mask.shape[0] - ti - 1
+                acc = (
+                    deltas[ti]
+                    + self.discounting
+                    * (1 - termination[ti])
+                    * truncation_mask[ti]
+                    * self.lambda_
+                    * acc
+                )
+                vs_minus_v_xs[ti] = acc
+
+            # Add V(x_s) to get v_s.
+            vs = vs_minus_v_xs + values
+            vs_t_plus_1 = torch.cat([vs[1:], torch.unsqueeze(bootstrap_value, 0)], 0)
+            advantages = (
+                reward + self.discounting * (1 - termination) * vs_t_plus_1 - values
+            ) * truncation_mask
+            return vs, advantages
+
+        @torch.jit.export
+        def loss(self, td: Dict[str, torch.Tensor]):
+            observation = self.normalize(td["observation"])
+            policy_logits = self.policy(observation[:-1])
+            baseline = self.value(observation)
+            baseline = torch.squeeze(baseline, dim=-1)
+
+            # Use last baseline value (from the value function) to bootstrap.
+            bootstrap_value = baseline[-1]
+            baseline = baseline[:-1]
+            reward = td["reward"] * self.reward_scaling
+            termination = td["done"] * (1 - td["truncation"])
+
+            loc, scale = self.dist_create(td["logits"])
+            behaviour_action_log_probs = self.dist_log_prob(loc, scale, td["action"])
+            loc, scale = self.dist_create(policy_logits)
+            target_action_log_probs = self.dist_log_prob(loc, scale, td["action"])
+
+            with torch.no_grad():
+                vs, advantages = self.compute_gae(
+                    truncation=td["truncation"],
+                    termination=termination,
+                    reward=reward,
+                    values=baseline,
+                    bootstrap_value=bootstrap_value,
+                )
+
+            rho_s = torch.exp(target_action_log_probs - behaviour_action_log_probs)
+            surrogate_loss1 = rho_s * advantages
+            surrogate_loss2 = (
+                rho_s.clip(1 - self.epsilon, 1 + self.epsilon) * advantages
+            )
+            policy_loss = -torch.mean(torch.minimum(surrogate_loss1, surrogate_loss2))
+
+            # Value function loss
+            v_error = vs - baseline
+            v_loss = torch.mean(v_error * v_error) * 0.5 * 0.5
+
+            # Entropy reward
+            entropy = torch.mean(self.dist_entropy(loc, scale))
+            entropy_loss = self.entropy_cost * -entropy
+
+            return policy_loss + v_loss + entropy_loss
+
+    class BayesianAgent(nn.Module):
+        """Standard PPO Agent with GAE and observation normalization."""
+
+        def __init__(
+            self,
             number_of_cell_types: int,
             policy_layers: Sequence[int],
             value_layers: Sequence[int],
@@ -326,6 +503,7 @@ def main(args):
 
     def train(
         seed,
+        is_weight_sharing: bool,
         number_of_cell_types: int,
         env_name: str = "ant",
         num_envs: int = 2048,
@@ -344,6 +522,7 @@ def main(args):
         progress_fn: Optional[Callable[[int, Dict[str, Any]], None]] = None,
     ):
         """Trains a policy via PPO."""
+        number_of_cell_types = int(number_of_cell_types)
         env = envs.create(
             env_name,
             batch_size=num_envs,
@@ -376,15 +555,26 @@ def main(args):
         config["compression_ratio"] = compression_ratio
 
         wandb.init(project="brax-cshl", config=config)
-        agent = Agent(
-            number_of_cell_types,
-            vanilla_policy_layers,
-            vanilla_value_layers,
-            entropy_cost,
-            discounting,
-            reward_scaling,
-            device,
-        )
+        if is_weight_sharing == True:
+            agent = BayesianAgent(
+                number_of_cell_types,
+                vanilla_policy_layers,
+                vanilla_value_layers,
+                entropy_cost,
+                discounting,
+                reward_scaling,
+                device,
+            )
+        elif is_weight_sharing == False:
+            agent = Agent(
+                vanilla_policy_layers,
+                vanilla_value_layers,
+                entropy_cost,
+                discounting,
+                reward_scaling,
+                device,
+            )
+
         agent = agent.to(device)
         optimizer = optim.Adam(agent.parameters(), lr=learning_rate)
 
@@ -490,6 +680,8 @@ def main(args):
         )
 
     train(
+        env_name=args.env_name,
+        is_weight_sharing=args.is_weight_sharing,
         number_of_cell_types=args.number_of_cell_types,
         progress_fn=progress,
         seed=int(args.seed),
@@ -504,36 +696,10 @@ def main(args):
     print(f"eval steps/sec: {np.mean(eval_sps)}")
     print(f"train steps/sec: {np.mean(train_sps)}")
 
-    # train_sps = []
-
-    # def progress(_, metrics):
-    #     if "training/sps" in metrics:
-    #         train_sps.append(metrics["training/sps"])
-
-    # ppo.train(
-    #     environment=envs.create(env_name="ant", backend="spring"),
-    #     num_timesteps=50_000_000,
-    #     num_evals=100,
-    #     reward_scaling=0.1,
-    #     episode_length=1000,
-    #     normalize_observations=True,
-    #     action_repeat=1,
-    #     unroll_length=5,
-    #     num_minibatches=32,
-    #     num_updates_per_batch=4,
-    #     discounting=0.97,
-    #     learning_rate=args.learning_rate,
-    #     entropy_cost=args.entropy_cost,
-    #     num_envs=args.number_envs,
-    #     batch_size=args.batch_size,
-    #     progress_fn=progress,
-    # )
-
-    # print(f"train steps/sec: {np.mean(train_sps[1:])}")
-
 
 if __name__ == "__main__":
     import argparse
+    from distutils.util import strtobool
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", default=0)
@@ -542,5 +708,13 @@ if __name__ == "__main__":
     parser.add_argument("--number_envs", default=2048)
     parser.add_argument("--batch_size", default=1024)
     parser.add_argument("--number_of_cell_types", default=5)
+    parser.add_argument(
+        "--is_weight_sharing",
+        type=lambda x: bool(strtobool(x)),
+        default=True,
+        nargs="?",
+        const=True,
+    )
+    parser.add_argument("--env_name", default="ant")
     args = parser.parse_args()
     main(args)
